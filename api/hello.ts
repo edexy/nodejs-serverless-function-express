adc24052620485128549181eed826b389d5328f9
Peter Edoja <edoja2012@gmail.com>
2024-10-22 17:26:33 +0100

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  return res.send("Oops, a little hiccup! We're ironing things out and will be back with you shortly!")
+  return res.send("Oops, a little hiccup! We're ironing things out and will be back with you shortly!");
 }
