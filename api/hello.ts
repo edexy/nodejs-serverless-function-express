ae7c004ad61c11e0ee95e8c4078cf93065e3cdae
Peter Edoja <edoja2012@gmail.com>
2024-10-22 17:53:46 +0100

    Delete api/hello.ts

--- api/hello.ts
+++
@@ -1,4 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return res.send("Oops, a little hiccup! We're ironing things out and will be back with you shortly!");
-}
